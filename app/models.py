496bdf349bb9cba5857070ecaeeac4021130bc3a
kishan <kishanlamba59@gmail.com>
2025-06-22 17:52:56 +0530

    user model password size changed

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     snippets = db.relationship('Snippet', backref='user', lazy=True)
 
     # a friendly respresentation of User object
